9a369cad368b0792e8d1d919e70f51d216c59d64
tetedacier <fabien.jacq@gmail.com>
2020-05-16 21:39:16 +0200

    test: cover rejection due to incorrect format and unavailability

--- src/gitModule.test.js
+++ src/gitModule.test.js
@@ -1,9 +1,10 @@
 const tap = require('tap')
 const expectedOutput = '[object Object]-test-true--lemon'
-
+const gitFs = require('./gitFs')
+const commitHash = 'c880d9c6ff751b57ddde30293fb3113f4489ec46'
 tap.test('gitModule should require the expected version of the fixture module', async (t) => {
     require('./gitModule')
-    const testedModule = await require('../__fixtures__/stringifyArray#c880d9c6ff751b57ddde30293fb3113f4489ec46.git')
+    const testedModule = await require(`../__fixtures__/stringifyArray#${commitHash}.git`)
 
     t.equal(
         testedModule([{a:1}, 'test', true, undefined, ['lemon']], '-'),
@@ -11,4 +12,45 @@ tap.test('gitModule should require the expected version of the fixture module', 
     )
 
     t.end()
+})
+
+const basePath = '__fixtures__/status';
+tap.test('gitModule should failed to require an incorect module', async (t) => {
+    const {
+        message: {
+            incorrectModuleName
+        }
+    } = require('./gitModule')
+    try {
+        await require(`../${basePath}#.git`)
+    }
+    catch (error){
+        t.equal(
+            error.constructor.name,
+            'Error'
+        )
+        t.equal(
+            error.message,
+            incorrectModuleName(`../${basePath}#.git`)
+        )
+        t.end()
+    }
+})
+tap.test('gitModule should failed to require a non resolved file', async (t) => {
+    require('./gitModule')
+    try {
+        await require(`../${basePath}#${commitHash}.git`)
+    }
+    catch (error){
+        console.warn(error)
+        t.equal(
+            error.constructor.name,
+            'Error'
+        )
+        t.equal(
+            error.message,
+            `Error: ${gitFs.noPathAtCurrentVersion(`./${basePath}.js`, commitHash)}`
+        )
+        t.end()
+    }
 })
